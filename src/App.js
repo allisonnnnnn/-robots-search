d9266356e73d433ef7cbb1fb1b86b0253f931ea8
allison <allisonchen1130@gmail.com>
2020-01-29 21:18:35 -0500

    search box complemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox";
+import CardList from "./CardList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    };
+  }
+
+  onSearchChange = e => {
+    this.setState({ searchfield: e.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    console.log(filteredRobots);
+    return (
+      <>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </>
+    );
+  }
 }
-
 export default App;
