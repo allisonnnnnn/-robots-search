76686ac63f40a00fedaa494735406c01d0029b17
allison <allisonchen1130@gmail.com>
2020-02-02 23:19:53 -0500

    containers vs components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
-import SearchBox from "./SearchBox";
-import CardList from "./CardList";
-import Scroll from "./Scroll";
+import SearchBox from "../components/SearchBox";
+import CardList from "../components/CardList";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
   constructor() {
@@ -24,13 +24,12 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-    if (this.state.robots.length === 0) {
+    if (robots.length === 0) {
       return <h1>Loading</h1>;
     } else {
       return (
