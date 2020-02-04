e229491f86da07acf14e776813e2f24a5a62b0e7
allison <allisonchen1130@gmail.com>
2020-02-03 23:19:40 -0500

    React-redux

---
+++ src/reducers.js
@@ -0,0 +1,17 @@
+import { CHANGE_SEARCH_FIELD } from "./constants";
+
+// Single source of truth
+const initialState = {
+  searchField: ""
+};
+
+// Reducer pure function
+export const searchRobots = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, { searchField: action.payload });
+    // return { ...state, searchField: action.payload };
+    default:
+      return state;
+  }
+};
